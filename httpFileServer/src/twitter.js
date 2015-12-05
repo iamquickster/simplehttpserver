04ccb5e5bbd6b8426aec5a9ffef88a0798b80be7
Quickster <Quickster@Quickster-PC>
2015-12-05 16:41:32 -0500

    delete tweet et afficher fil fonctionelle
    doit etre styliser

--- httpFileServer/src/twitter.js
+++ httpFileServer/src/twitter.js
@@ -5,12 +5,12 @@ function getUser(userId) {
 	httpRequest.onreadystatechange = function() {
 		if(httpRequest.readyState == 4) {
 			if(httpRequest.status == 200) {
-				document.getElementById("mainViewPort").innerHTML = "";				
+				document.getElementById("menu").innerHTML += "<div id='suggestions' class='suggestionList'></div>";				
 				users = JSON.parse(httpRequest.responseText);
 				
 				console.log(users);
 				for(var i = 0 ; i < users.length; i++) {
-					document.getElementById("mainViewPort").innerHTML += "<div onclick='loadUser(\"" + users[i].name + "\",\"" + users[i].link + "\")'>" + users[i].name +"</div>";
+					document.getElementById("suggestions").innerHTML += "<a href='#' onclick='loadUser(\"" + users[i].name + "\",\"" + users[i].link + "\")'>" + users[i].name +"</a>";
 					
 				}
 				
@@ -23,7 +23,8 @@ function getUser(userId) {
 }
 
 function loadUser( userName, link ) {
-	document.getElementById("mainViewPort").innerHTML = "<div><h1>" + userName + "</h1></div>";
+	document.getElementById("suggestions").innerHTML = "";
+	document.getElementById("user-content").innerHTML = "<div id=\"userName\"><h1>" + userName + "</h1></div>" + document.getElementById("user-content").innerHTML;
 	var httpRequest = new XMLHttpRequest();
 	httpRequest.onreadystatechange = function() {
 		if(httpRequest.readyState == 4) {
@@ -31,11 +32,16 @@ function loadUser( userName, link ) {
 				var feed = JSON.parse(httpRequest.responseText);
 				
 				console.log(feed);
+				document.getElementById("tweets").innerHTML = "";
 				for(var i = 0; i < feed.length ; i++ ) {
 					if(feed[i].tweet) {
-						document.getElementById("mainViewPort").innerHTML += "<div class='reTweet'>" + feed[i].tweet.date + ":" + feed[i].owner + "-->" + feed[i].tweet.message + "</div>";
+						document.getElementById("tweets").innerHTML += "<li class='list-group-item reTweet' id=tweetId-" 
+							+ feed[i].id + ">" + feed[i].tweet.date + ":"
+							+ feed[i].owner + "-->" + feed[i].tweet.message 
+							+ "<a onclick=\"deleteTweet(" + feed[i].id+ ")\"><span class=\"glyphicon glyphicon-remove\"></span></a>"
+							+ "</li>";
 					} else {
-						document.getElementById("mainViewPort").innerHTML += "<div class='tweet'>" + feed[i].date + ":" + feed[i].message + "</div>";
+						document.getElementById("tweets").innerHTML += "<li class='list-group-item tweet' id=\"tweetId-" + feed[i].id + "\">" + feed[i].date + ":" + feed[i].message + "</li>";
 					}
 				}
 				// TODO Create user dashboard
@@ -46,5 +52,27 @@ function loadUser( userName, link ) {
 	}
 	
 	httpRequest.open('GET', link);
+	httpRequest.send();
+}
+
+function deleteTweet(tweetId) {
+
+	var userName = document.getElementById("userName");
+	var tweet = document.getElementById("tweetId-" + tweetId);
+	if(tweet.class == "tweet") {
+		var link = "/utilisateurs/" + userName.textContent + "/tweets/" + tweet.getAttribute('id').replace("tweetId-", "");
+	} else {
+		var link = "/utilisateurs/" + userName.textContent + "/retweets/" + tweet.getAttribute('id').replace("tweetId-", "");
+	}
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.onreadystatechange = function() {
+		if(httpRequest.readyState == 4) {
+			if(httpRequest.status == 200) {
+				tweet.remove();
+			}
+		}
+	}
+	
+	httpRequest.open('DELETE', link);
 	httpRequest.send();
 }
