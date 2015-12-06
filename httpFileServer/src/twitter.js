72507f8bb782f087cfcd94661c5ae8fbf718a897
nicolaw <nicolaw@nicolaw-laptop>
2015-12-06 16:48:09 -0500

    affichage des followees

--- httpFileServer/src/twitter.js
+++ httpFileServer/src/twitter.js
@@ -21,7 +21,10 @@ function getUser(userId) {
 	httpRequest.open('GET', '/utilisateurs/' + userId + '/fil/');
 	httpRequest.send();
 }
-
+function loadUser( userName ) {
+	var link='/utilisateurs/' + userName + '/fil/';
+	loadUser( userName, link );
+}
 function loadUser( userName, link ) {
 	document.getElementById("suggestions").innerHTML = "";
 	document.getElementById("user-picked").innerHTML = "<h1>" + userName + "</h1>";
@@ -46,6 +49,33 @@ function loadUser( userName, link ) {
 				}
 				// TODO Create user dashboard
 				// functions : show feed + buttons to tweet and retweet
+				
+			}
+		}
+	}
+	
+	httpRequest.open('GET', link);
+	httpRequest.send();
+	loadFollows();
+}
+
+function loadFollows() {
+	var userName = document.getElementById("user-picked").textContent;
+	var link = "/utilisateurs/" + userName + "/abonnements/";
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.onreadystatechange = function() {
+		if(httpRequest.readyState == 4) {
+			if(httpRequest.status == 200) {
+				var feed = JSON.parse(httpRequest.responseText);
+				
+				console.log(feed);
+				document.getElementById("abonnements").innerHTML = "";
+				for(var i = 0; i < feed.length ; i++ ) {				
+					document.getElementById("abonnements").innerHTML += "<li class='list-group-item followee' id=followeeId-" 
+						+ feed[i].name + ">" + feed[i].name +
+						"<a onclick=\"unfollow(" + feed[i].name+ ")\"><span class=\"glyphicon glyphicon-remove\"></span></a>"
+						+ "</li>";
+				}
 				
 			}
 		}
